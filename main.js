4333327090226d01fb159786bc467a59d8fef6ab
megabanana23 <65601935+megabanana23@users.noreply.github.com>
2021-09-16 12:07:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+greencar_width = 75;
+greencar_height = 100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+greencar_x = 5;
+greencar_y= 225;
+
+function add() {
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image; ;
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
+	;
+	}
+
+	function uploadBackground() {
+		ctx.drawImage(background_imgTag, a, e, canvas.width, canvas.height);
+		}
+
+		function uploadgreencar() {
+			ctx.drawImage(greencar_imgTag, greencar_x,greencar_y, preencar_width, greencar_height);
+
+		}
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up())
+{
+if(greencary >=0)
+{
+greencar_y = greencar_y - 10;
+console.log("When up arrow is pressed, X = " + greencar_x + I y = " +greencar_y);
+uploadBackground();
+uploadgreencar();
+
+function down()
+{
+	function up())
+{
+if(greencary >=0)
+{
+greencar_y = greencar_y + 10;
+console.log("When up arrow is pressed, X = " + greencar_x + I y = " +greencar_y);
+uploadBackground();
+uploadgreencar();
+}
+
+function left()
+{
+	function up())
+{
+if(greencary >=0)
+{
+greencar_x = greencar_x - 10;
+console.log("When up arrow is pressed, X = " + greencar_x + I y = " +greencar_y);
+uploadBackground();
+uploadgreencar();
+}
+
+function right()
+{
+	function up())
+{
+if(greencary >=0)
+{
+greencar_y = greencar_y + 10;
+console.log("When up arrow is pressed, X = " + greencar_x + I y = " +greencar_y);
+uploadBackground();
+uploadgreencar();
+}
